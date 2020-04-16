abd159ae8a5290a8bc27b0b1d58f809659390db4
mmares <myrnamares90@gmail.com>
2020-04-16 16:42:45 -0500

    Se agregan cambios en los diseños del componente crear boletines, hover en los iconos de info, el input para adjunta imagenes

--- src/Componentes/Componentes-boletines/FormBoletines.js
+++ src/Componentes/Componentes-boletines/FormBoletines.js
@@ -18,7 +18,9 @@ class FormBoletines extends Component {
             solucion: '',
             imgSolucion: '',
             usuario: '',
-            departamento: ''
+            departamento: '',
+            imagProb: '',
+            imagSolu: ''
         }
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
@@ -37,6 +39,52 @@ class FormBoletines extends Component {
 
     handleFormReset() {
         this.setState(() => this.state)
+    }
+
+    imgUp = () => {
+
+        const file = this.refs.uploadImg.files[0]
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            this.setState({
+                imagProb: reader.result
+            })
+        }
+        if (file) {
+            reader.readAsDataURL(file);
+            this.setState({
+                imagProb: reader.result
+            })
+        }
+        else {
+            this.setState({
+                imagProb: ""
+            })
+        }
+    }
+
+    imgUp2 = () => {
+
+        const file = this.refs.uploadImg2.files[0]
+        const reader = new FileReader();
+
+        reader.onloadend = () => {
+            this.setState({
+                imagSolu: reader.result
+            })
+        }
+        if (file) {
+            reader.readAsDataURL(file);
+            this.setState({
+                imagSolu: reader.result
+            })
+        }
+        else {
+            this.setState({
+                imagSolu: ""
+            })
+        }
     }
 
     render() {
@@ -66,8 +114,15 @@ class FormBoletines extends Component {
                         </div>
                         <div className="contenedor-form-div2">
                             <label className="label label1">Proyecto</label>
+                            <div className="div-icon-info">
                             <select className="input input1" name="proyecto" value={proyecto} onChange={this.handleChange} required></select>
-
+                          
+                            <a className="caja3">
+                           <span className="material-icons md-4">info</span>
+                           <span className="info">Seleccionar proyecto <br/>donde ocurrio el problema</span>
+                           </a>
+                            </div>
+                            
 
 
                             <label className="label label2">Cliente</label>
@@ -76,12 +131,18 @@ class FormBoletines extends Component {
 
 
                             <label className="label label3">Fecha</label>
-                            <input className="input input3 input-fecha" name="fecha" type="date" value={fecha} onChange={this.handleChange} required></input>
+                            <input className="input input3 input-fecha" name="fecha" type="month" pattern="[0-9]{4}-[0-9]{2}-[0-9]{2}" value={fecha} onChange={this.handleChange} required></input>
 
 
                             <label className="label label4">Sistema</label>
+                            <div className="div-icon-info">
                             <select className="input input4" name="sistema" value={sistema} onChange={this.handleChange} required></select>
-
+                            <a className="caja3">
+                           <span className="material-icons md-4">info</span>
+                           <span className="info">Seleccionar proyecto <br/>donde ocurrio el problema</span>
+                           </a>
+                            </div>
+                            
                         </div>
 
                     </div>
@@ -95,33 +156,64 @@ class FormBoletines extends Component {
 
                             <div className="contenForm">
                                 <label className="label label-text-area">Descripción del problema</label>
-                                <textarea className=" text-problema text" name="problema" value={problema} onChange={this.handleChange} required></textarea>
+                                <textarea className=" text-problema text" name="problema" value={problema}
+                                    onChange={this.handleChange} required></textarea>
                             </div>
                             <div className="contenForm2">
                                 <label className="label label-text-area ">Solución del problema</label>
-                                <textarea className=" text-solucion text" name="solucion" value={solucion} onChange={this.handleChange} required></textarea>
+                                <textarea className=" text-solucion text" name="solucion" value={solucion}
+                                    onChange={this.handleChange} required></textarea>
                             </div>
 
 
                             <div className="contenForm conten-img">
-                                <label className="label label-img">Problema imagen</label>
-                                <div className=" conten-export-img" type="file" required><input type="file" name="attachment" className="inputimg" /><i className="img-export" class="material-icons md-50">add_photo_alternate</i></div>
+                            <label className="label label-img">Solución imagen</label>
+                                <input ref="uploadImg"
+                                    type="file"
+                                    name="selectedFile"
+                                    onChange={this.imgUp} className="inputimg" />
+                                <img src={this.state.imagProb} className=" conten-export-img" required />
                             </div>
 
                             <div className="contenForm conten-img2">
                                 <label className="label label-img">Solución imagen</label>
-                                <div className=" conten-export-img" type="file" required><input type="file" name="attachment" className="inputimg" /><i className="img-export" class="material-icons md-50">add_photo_alternate</i></div>
+                                <input ref="uploadImg2"
+                                    type="file"
+                                    name="selectedFile"
+                                    onChange={this.imgUp2} className="inputimg" />
+                                <img src={this.state.imagSolu} className=" conten-export-img" required />
                             </div>
 
                             <div className="content-inputs">
                                 <label className=" label label-div">Equipo</label>
+                                <div className="div-icon-info">
                                 <input className="input input-div" name="equipo" value={equipo} onChange={this.handleChange}></input>
-
+                                <a className="caja3">
+                           <span className="material-icons md-4">info</span>
+                           <span className="info">Seleccionar proyecto <br/>donde ocurrio el problema</span>
+                           </a>
+                                </div>
+                                
+                                
                                 <label className=" label label-div2">Marca</label>
+                                <div className="div-icon-info">
                                 <input className="input input-div2" name="marca" value={marca} onChange={this.handleChange}></input>
-
+                                <a className="caja3">
+                           <span className="material-icons md-4">info</span>
+                           <span className="info">Seleccionar proyecto <br/>donde ocurrio el problema</span>
+                           </a>
+                           </div>
+                            
+                                
                                 <label className=" label label-div3">Modelo</label>
+                                <div className="div-icon-info">
                                 <input className="input input-div3" name="modelo" value={modelo} onChange={this.handleChange}></input>
+                                <a className="caja3">
+                           <span className="material-icons md-4">info</span>
+                           <span className="info">Seleccionar proyecto <br/>donde ocurrio el problema</span>
+                           </a>
+                           </div>
+                                
                             </div>
                         </div>
                     </div>
@@ -133,12 +225,26 @@ class FormBoletines extends Component {
                         </div>
                         <div className="contenForm conteinerForm-2">
                             <label className="label">Reporta</label>
+                            <div className="div-icon-info">
                             <input className="input" name="usuario" value={usuario} onChange={this.handleChange} required></input>
+                            <a className="caja3">
+                           <span className="material-icons md-4">info</span>
+                           <span className="info">Seleccionar proyecto <br/>donde ocurrio el problema</span>
+                           </a>
+                           </div>
+                           
                         </div>
 
                         <div className="contenForm conteinerForm-3">
                             <label className="label">Departamento</label>
+                            <div className="div-icon-info">
                             <input className="input" name="departamento" value={departamento} onChange={this.handleChange} required></input>
+                            <a className="caja3">
+                           <span className="material-icons md-4">info</span>
+                           <span className="info">Seleccionar proyecto <br/>donde ocurrio el problema</span>
+                           </a>
+                           </div>
+                            
                         </div>
                     </div>
                 </form>
