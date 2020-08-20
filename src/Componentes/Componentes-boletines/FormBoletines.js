8911d7d4479ca7906ec7375ba96f5d8566e9227b
myrna90 <myrnamares90@gmail.com>
2020-08-20 11:29:31 -0500

    Cambios función post boletines

--- src/Componentes/Componentes-boletines/FormBoletines.js
+++ src/Componentes/Componentes-boletines/FormBoletines.js
@@ -5,33 +5,9 @@ import FormStepThree from "./Steps/FormStepThree";
 import FormStepFour from "./Steps/FormStepFour";
 
 
-
-
 const FormBoletines = (props) => {
   /*Es un destructure al obejeto props y sacamos esa variable, para no tener que hacer props handleChange*/
   const { handleChange, handleSubmit } = props;
-
-  /*useEffect(() => {
-    fetch('http://localhost:3000/api/boletines')
-      .then((res) => res.json())
-      .then((res) => setFormValues(res.rows))
-      .catch((err) => console.log('error'))
-  }, []);*/
-
-  
-  /*const handleSubmit = (event) => {
-    fetch('http://localhost:3000/api/boletines/add', {
-      method: 'POST',
-      body: JSON.stringify(formData),
-    })
-      .then((res) => res.json())
-      .then((result) => {
-        console.log(result)
-        setFormData('')
-      })
-      .catch((err) => console.log('error'));
-      console.log(handlerSubmit());
-  }*/
 
   return (
     <div className="contenido section ">
