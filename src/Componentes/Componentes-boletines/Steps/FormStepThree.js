1cab4b879efd88c74cc081fdc72185a4526c7319
rubent0rres <dev@rubentorres.com.mx>
2020-10-04 21:00:08 -0500

    Subject: add minLength text areas Description&Solution

--- src/Componentes/Componentes-boletines/Steps/FormStepThree.js
+++ src/Componentes/Componentes-boletines/Steps/FormStepThree.js
@@ -1,38 +1,39 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 const FormStepThree = (props) => {
   const { handleChange } = props;
   const { deviceData } = props;
 
   const [formImage, setFormImage] = useState({
-    device: "",
-    brand: "",
-    model: "",
-    description: "",
+    device: '',
+    brand: '',
+    model: '',
+    description: '',
     //imgProblema: "",
-    solution: "",
+    solution: '',
     //imgSolucion: ""
   });
 
   return (
     /*parte del formulario para descripcion y solución del problema */
-    <div className="div div-3">
+    <div className='div div-3'>
       {/*Div contenido espacio descripción, imagenes y solución */}
-      <div className="conteiner-numero numero-div3">
-        <div className="numero">3</div>
+      <div className='conteiner-numero numero-div3'>
+        <div className='numero'>3</div>
         <strong>
-          <p className="p">Descripción</p>
+          <p className='p'>Descripción</p>
         </strong>
       </div>
-      <div className="div1 div3-content">
+      <div className='div1 div3-content'>
         {/*Div contenido espacio descripción problema*/}
-        <div className="contenForm">
-          <label className="label label-text-area">
+        <div className='contenForm'>
+          <label className='label label-text-area'>
             Descripción del problema
           </label>
           <textarea
-            className=" text-problema text"
-            name="description"
+            className=' text-problema text'
+            name='description'
+            minLength={10}
             defaultValue={formImage.description}
             onChange={(e) => handleChange(e)}
             required
@@ -40,40 +41,42 @@ const FormStepThree = (props) => {
         </div>
 
         {/*Div contenido espacio descripción solución*/}
-        <div className="contenForm2">
-          <label className="label label-text-area ">
+        <div className='contenForm2'>
+          <label className='label label-text-area '>
             Solución del problema
           </label>
           <textarea
-            className=" text-solucion text"
-            name="solution"
+            className=' text-solucion text'
+            name='solution'
+            minLength={10}
             defaultValue={formImage.solution}
             onChange={(e) => handleChange(e)}
             required
           ></textarea>
         </div>
 
-       
-
         {/*Div contenido inputs equipo, modelo, marca */}
-        <div className="content-inputs">
+        <div className='content-inputs'>
           {/*Input equipo */}
-          <label className=" label label-div">Equipo</label>
-          <div className="div-icon-info">
+          <label className=' label label-div'>Equipo</label>
+          <div className='div-icon-info'>
             <select
-              className="input input-div"
-              name="device"
+              className='input input-div'
+              name='device'
               defaultValue={formImage.device}
               onChange={(e) => handleChange(e)}
             >
               <option>-</option>
-              {deviceData && deviceData.map((device, index) => (
-                <option key={index} value={device._id}>{device.name}</option>
-              ))}
+              {deviceData &&
+                deviceData.map((device, index) => (
+                  <option key={index} value={device._id}>
+                    {device.name}
+                  </option>
+                ))}
             </select>
-            <a className="caja3">
-              <span className="material-icons md-4">info</span>
-              <span className="info">
+            <a className='caja3'>
+              <span className='material-icons md-4'>info</span>
+              <span className='info'>
                 Seleccionar proyecto <br />
                 donde ocurrio el problema
               </span>
@@ -81,22 +84,25 @@ const FormStepThree = (props) => {
           </div>
 
           {/*Input marca */}
-          <label className=" label label-div2">Marca</label>
-          <div className="div-icon-info">           
+          <label className=' label label-div2'>Marca</label>
+          <div className='div-icon-info'>
             <select
-              className="input input-div2"
-              name="brand"
+              className='input input-div2'
+              name='brand'
               defaultValue={formImage.brand}
               onChange={(e) => handleChange(e)}
             >
               <option>-</option>
-              {deviceData && deviceData.map((device, index) => (
-                <option key={index} value={device._id}>{device.brand}</option>
-              ))}
-              </select>
-            <a className="caja3">
-              <span className="material-icons md-4">info</span>
-              <span className="info">
+              {deviceData &&
+                deviceData.map((device, index) => (
+                  <option key={index} value={device._id}>
+                    {device.brand}
+                  </option>
+                ))}
+            </select>
+            <a className='caja3'>
+              <span className='material-icons md-4'>info</span>
+              <span className='info'>
                 Seleccionar proyecto <br />
                 donde ocurrio el problema
               </span>
@@ -104,22 +110,25 @@ const FormStepThree = (props) => {
           </div>
 
           {/*Input modelo */}
-          <label className=" label label-div3">Modelo</label>
-          <div className="div-icon-info">
+          <label className=' label label-div3'>Modelo</label>
+          <div className='div-icon-info'>
             <select
-              className="input input-div3"
-              name="model"
+              className='input input-div3'
+              name='model'
               defaultValue={formImage.model}
               onChange={(e) => handleChange(e)}
             >
               <option>-</option>
-               {deviceData && deviceData.map((device, index) => (
-                <option key={index} value={device._id}>{device.model}</option>
-              ))}
+              {deviceData &&
+                deviceData.map((device, index) => (
+                  <option key={index} value={device._id}>
+                    {device.model}
+                  </option>
+                ))}
             </select>
-            <a className="caja3">
-              <span className="material-icons md-4">info</span>
-              <span className="info">
+            <a className='caja3'>
+              <span className='material-icons md-4'>info</span>
+              <span className='info'>
                 Seleccionar proyecto <br />
                 donde ocurrio el problema
               </span>
@@ -132,4 +141,3 @@ const FormStepThree = (props) => {
 };
 
 export default FormStepThree;
-
