24e4f236d81e5674621c7608ffda72315a1f85bf
myrna90 <myrnamares90@gmail.com>
2020-10-10 08:41:12 -0500

    Cambios

--- src/Componentes/Componentes-Listado/searchBar.js
+++ src/Componentes/Componentes-Listado/searchBar.js
@@ -5,41 +5,66 @@ import { API_BASE_URL } from "../../configuration";
 import AuthService from "../../Componentes/Componentes-login/service/auth.service";
 
 const SearchBar = (props) => {
-  const [newslettersData, setNewslettersData] = useState({data: []});
   const token = AuthService.getCurrentUser();
   const [search, setSearch] = useState("");
   const [searchResults, setSearchResults] = useState([]);
+  const [newslettersData, setNewslettersData] = useState(undefined);
 
   const newsGet = {
-    method: "GET",
+    method: 'GET',
     url: `${API_BASE_URL}/newsletters`,
     headers: {
       Authorization: `Bearer ${token.token}`,
     },
   };
-  
+
   useEffect(() => {
-    console.log('data', newslettersData);
-    if (newslettersData === []) {
-      axios(newsGet).then((res) => {
+    if (newslettersData === undefined) {
+      axios(newsGet).then(function(res) {
         setNewslettersData(res.data.data);
       });
     }
   }, [newslettersData]);
 
-  const handleChange = (event) => {
-    setSearchResults(event.target.value);
-    console.log("object", event.target.value);
-  };
+  console.log(newslettersData);
 
-  useEffect(() => {
-    const results = newslettersData.filter((boletin) =>
-     boletin.project.toLowerCase().includes(search.toLocaleLowerCase()) 
-    );
-    setSearchResults(results);
-  }, [search]);
+  const searchResultMap = () => {
+    searchResults.map(boletin => {
+      return (
+       <div>
+          <tr className="tr-general">
+          <td>{boletin.folio}</td>
+          
+          <td>{boletin.project[0].name}</td>
+          <td>{boletin.system[0].name}</td>
+          <td>
+            {boletin.owner[0].firstname} {boletin.owner[0].lastname}
+          </td>
+          <td>
+            <Link
+                  to={{
+                    pathname: '/vista/view/',
+                    state: {
+                      id: `${boletin._id}`,
+                    },
+                  }}
+                  className='btn-list'
+                >
+                  <i className='material-icons  md-19'>open_in_new</i>
+                </Link>
+          </td>
+        </tr> 
+       </div>
+      )
+    })
+  } 
+  // const handleChange = (event) => {
+  //   let keyword = event.target.value;
+  //   setSearch({search: keyword})
+    // setSearchResults(event.target.value);
+    // console.log("object", event.target.value);};
+  
 
-console.log('result', searchResults);
   return (
     <div>
       <div className="div-filtro">
@@ -49,7 +74,7 @@ console.log('result', searchResults);
           type="text"
           placeholder="Filtro"
           defaultValue={search}
-          onChange={(event) => handleChange(event)}
+          onChange={(e) => setSearchResults(e.target.value)}
         />
       </div>
       <div className="div-tabla">
@@ -73,33 +98,10 @@ console.log('result', searchResults);
               </th>
             </tr>
           </thead>
-          {/* <tbody>
-            {newslettersData &&
-              newslettersData.map((boletines, index) => (
-                <tr key={index} className="tr-general">
-                  <td>{boletines.folio}</td>
-                  
-                  <td>{boletines.project[0].name}</td>
-                  <td>{boletines.system[0].name}</td>
-                  <td>
-                    {boletines.owner[0].firstname} {boletines.owner[0].lastname}
-                  </td>
-                  <td>
-                    <Link
-                          to={{
-                            pathname: '/vista/view/',
-                            state: {
-                              id: `${filterFolio._id}`,
-                            },
-                          }}
-                          className='btn-list'
-                        >
-                          <i className='material-icons  md-19'>open_in_new</i>
-                        </Link>
-                  </td>
-                </tr>
-              ))}
-          </tbody> */}
+          <tbody>
+          {/* */}
+          {searchResultMap}
+          </tbody>
         </table>
       </div>
     </div>
